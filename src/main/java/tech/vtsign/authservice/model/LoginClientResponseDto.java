4295db24e11520addb0ea4d872bddbe6b1556b4c
ngthotuan <ngthotuan@gmail.com>
2021-12-18 16:43:27 +0700

    response roles, permissions to login user

--- src/main/java/tech/vtsign/authservice/model/LoginClientResponseDto.java
+++ src/main/java/tech/vtsign/authservice/model/LoginClientResponseDto.java
@@ -6,6 +6,7 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.util.List;
 import java.util.UUID;
 
 @Data
@@ -30,4 +31,6 @@ public class LoginClientResponseDto {
     private boolean blocked;
     private long balance;
     private String avatar;
+    private List<Role> roles;
+    private List<Permission> permissions;
 }
