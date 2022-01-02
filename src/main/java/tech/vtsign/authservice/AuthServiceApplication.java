af964b21d7cba9b40009caa8834c60d61092914d
ngthotuan <ngthotuan@gmail.com>
2022-01-02 23:00:43 +0700

    update auth-service env

--- src/main/java/tech/vtsign/authservice/AuthServiceApplication.java
+++ src/main/java/tech/vtsign/authservice/AuthServiceApplication.java
@@ -1,14 +1,9 @@
 package tech.vtsign.authservice;
 
-import io.swagger.v3.oas.models.OpenAPI;
-import io.swagger.v3.oas.models.info.Info;
-import io.swagger.v3.oas.models.servers.Server;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
-import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 @EnableEurekaClient
@@ -17,15 +12,5 @@ public class AuthServiceApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(AuthServiceApplication.class, args);
-    }
-
-    @Bean
-    public OpenAPI customOpenAPI(@Value("${springdoc.version}") String appVersion) {
-        return new OpenAPI()
-                .info(new Info().title("Auth Service API")
-                        .description("API documentation Auth Service")
-                        .version(appVersion)
-                )
-                .addServersItem(new Server().url("https://api.vtsign.tech/auth"));
     }
 }
--- src/main/java/tech/vtsign/authservice/configuration/GlobalConfiguration.java
+++ src/main/java/tech/vtsign/authservice/configuration/GlobalConfiguration.java
@@ -1,14 +1,32 @@
 package tech.vtsign.authservice.configuration;
 
 import feign.codec.ErrorDecoder;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.servers.Server;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import tech.vtsign.authservice.proxy.FeignCustomErrorDecoder;
 
 @Configuration
 public class GlobalConfiguration {
+    @Value("${tech.vtsign.api-url}")
+    private String apiUrl;
+    @Value("${springdoc.swagger-uri.path}")
+    private String path;
     @Bean
     public ErrorDecoder errorDecoder() {
         return new FeignCustomErrorDecoder();
     }
+    @Bean
+    public OpenAPI customOpenAPI(@Value("${springdoc.version}") String appVersion) {
+        return new OpenAPI()
+                .info(new Info().title("Auth Service API")
+                        .description("API documentation Auth Service")
+                        .version(appVersion)
+                )
+                .addServersItem(new Server().url(apiUrl+path));
+    }
+
 }
